72a56b700ee02635a047f42acfe34f63d0e97be8
Ewan Nicolson <ewan.nicolson@gmail.com>
2013-02-10 20:48:09 +0000

    making the python package correct

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@ from distutils.core import setup
 
 setup(
     name="FranticAccelerator",
-    version='0.1.0',
+    version='0.1',
     author='Ewan Nicolson',
-    packages=['franticaccelerator'],
+    author_email = 'ewannic@gmail.com',
+    packages=['FranticAccelerator'],
     scripts=[],
     url='https://github.com/dataewan/frantic-accelerator',
-    licence='LICENCE.txt',
+    license='LICENCE.txt',
     description='Static document generator using jinja2 and yaml',
-    long_description=open("README.rst").read(),
-    install_requires=[
-    ],
+    long_description=open("README").read(),
+    install_requires = [
+        "jinja2",
+        "markdown",
+        "pyyaml",
+        "docutils"
+    ]
 )
